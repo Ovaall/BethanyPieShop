64955953ef3d9b80cd51077383218a6c1039bd5d
Valentyn Tukov <valentin.tukov@gmail.com>
2023-08-21 17:21:27 +0200

    first commit

---
+++ BethanyPieShopHRM/Program.cs
@@ -0,0 +1,173 @@
+﻿
+
+//Console.WriteLine("Welcome to Bethany's Pie Shop HRS");
+
+//Console.WriteLine("Please enter your name: ");
+
+//string name = Console.ReadLine();
+
+//int monthlyWage = 1234;
+
+//int month = 12; int bonus = 1000;
+
+//bool isActive = true;
+
+//double rating = 99.25;
+
+//byte numberOfEmployees = 125;
+
+//int hoursWorked;
+
+//    hoursWorked = 125;
+
+//hoursWorked = 148;
+
+//const double interestRate = 0.07;
+
+//string firstName = "Bethany";
+
+//string lastName = "Smith";
+
+//string emtyString = "";
+
+
+//double ratePerHour = 12.34;
+
+//int numberOfHoursWage = 165;
+
+//double bonus = 18;
+
+//double currentMonthWage = ratePerHour * numberOfHoursWage + bonus;
+
+//Console.WriteLine(currentMonthWage);
+
+//ratePerHour += 3; // ratePerHour = ratePerHour +3;
+
+//Console.WriteLine(ratePerHour);
+
+//if (currentMonthWage > 2000)
+//    Console.WriteLine("Top paid employee");
+
+//using System;
+
+//int intMaxValue = int.MaxValue;
+
+//int intMinValue = int.MinValue;
+
+//char userSelection = 'a';
+//char upperVersion = char.ToUpper(userSelection);    
+
+//bool isDigit = char.IsDigit(upperVersion);
+
+//bool isLetter = char.IsLetter(upperVersion);
+
+//Console.WriteLine(isLetter);
+//Console.WriteLine(isDigit);
+
+//using System.Data;
+
+//DateTime hireDate = new DateTime(2023, 05, 21, 09, 30, 0);
+//Console.WriteLine(hireDate);
+
+
+//DateTime exitDate = new DateTime(2028, 05, 31);
+//Console.WriteLine(exitDate);
+
+//DateTime startDate = hireDate.AddDays(15);
+//Console.WriteLine(startDate);
+
+//DateTime currentDate = DateTime.Now;
+////bool areWeInDst = currentDate.IsDayLightSavingTime();
+////Console.WriteLine(areWeInDst);
+//Console.WriteLine(currentDate);
+
+
+
+//DateTime startHour = DateTime.Now;
+//TimeSpan workTime = new TimeSpan(8, 0, 0);
+//DateTime endHour = startHour.Add(workTime);
+
+//Console.WriteLine(startHour.ToLongDateString());
+//Console.WriteLine(endHour.ToShortTimeString());
+
+//int numberOfHoursWorked = 165;
+
+//long veryLongMonth = numberOfHoursWorked;
+
+//double d = 123456789.0;
+
+//int x = (int)d;
+
+//int intVeryLongMonth = (int)veryLongMonth;
+
+//var a = 123;
+//var b = true;
+//var d = 123.0;
+
+
+//Console.WriteLine("Enter a first value: ");
+//string stringValue1 = Console.ReadLine();
+
+//Console.WriteLine("Emter a second value: ");
+//string stringValue2 = Console.ReadLine();
+
+//int intValue1 = int.Parse(stringValue1);
+//int intValue2 = int.Parse(stringValue2);
+
+//var sum = intValue1 + intValue2;
+//Console.WriteLine("The sum is: " +  sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
